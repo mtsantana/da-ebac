80da163541e9f5ada024d1fd166801c3389618bf
mtsantana <marcotulioscs_91@hotmail.com>
2023-01-17 18:55:45 +0000

    alteração no código para geração do gráfico do preço da gasolina

--- gasolina.py
+++ gasolina.py
@@ -2,12 +2,11 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-
 df = pd.read_csv('gasolina.csv')
 
-with sns.axes_style('whitegrid'):
-  grafico = sns.lineplot(data = df, x = 'dia', y = 'venda')
-  grafico.set(title = 'Preço da gasolina ao longo dos dias', \
-            xlabel = 'Dia', ylabel = "Preço")
+grafico = sns.lineplot(data = df, x = 'dia', y = 'venda', palette = 'pastel')
+
+grafico.set(title = 'Preço da gasolina - Julho 2021',\
+            xlabel = 'Dia da consulta', ylabel = 'Preço de venda (R$)')
 
 plt.savefig('gasolina.png')
